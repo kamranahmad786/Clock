666bc34308ecf982a83fe3eb0e6e74a0057e7457
kamran <mohammadkamranahmad786@gmail.com>
2024-07-29 14:36:43 +0000

    solution

---
+++ src/components/Clock/index.js
@@ -0,0 +1,27 @@
+import {useState, useEffect} from 'react'
+
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => clearInterval(timerId)
+  }, [])
+
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time>{date.toLocaleTimeString()}</Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
